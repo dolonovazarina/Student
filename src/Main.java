f6adc4071d79373dbf1fc3b1e7e14990c7d46281
dolonovazarina <dolonovazarina87@gmail.com>
2023-01-16 20:57:36 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+
+//     Rectangle student =new Rectangle();
+//      student.setName("Zarina");
+//       student.setSurname("Dolonova");
+//       student.setEmail("dolonovazarina@");
+//       student.setAge( 35);
+//        System.out.println( "Name:" +student.getName()+"Surname:"+student.getSurname()
+//                +"Age:" +student.getAge()+"Email:"+student.getEmail());
+// Конс жообу  Zarina Dolonova 35 dolonovazarina
+
+
+
+
+
+        //   Rectangle student  = new Rectangle();
+        //   student.nameSaluu("Zara");
+        //  student.ageAluu ( 120);
+        //  System.out.println(student.nameAluu());
+        //  System.out.println(student.ageAluu());
+
+        Scanner scanner= new Scanner(System.in);
+        Rectangle rectangle = new Rectangle();
+        rectangle.setA
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,82 @@
+public class Rectangle {
+//    private String name;
+//    private int age;
+//
+//    private String email;
+//
+//    private String surname;
+//
+//      System.out.println("tuura emes jash");
+//
+//    public String getSurname() {
+//        return surname;
+//    }
+
+//    public void setSurname(String surname) {
+//        this.surname = surname;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+
+
+    //  public  void  nameSaluu(String aty){
+    // name=aty;
+    // }
+    // public String ageAluu(){
+    //   return  name;
+    // }
+    //public  void  ageAluu(int jash){
+    //  if (jash<100){
+    //      age=jash;
+    //  }
+    // else {
+    //    System.out.println("tuura emes jash");
+
+    private  int a;
+     private   int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
+
+
+
+
+
+
+
+
